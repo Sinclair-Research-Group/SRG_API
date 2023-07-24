ad7a300fca18349d6d079f947634e4ebf6c17770
Sinclair-Research-Group <alana@sinclairgroup.org>
2023-07-24 15:54:16 -0700

    update ssl

--- config/ssl/ssl.js
+++ config/ssl/ssl.js
@@ -1,10 +1,14 @@
 const https = require('https');
-// const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
 module.exports = function (api) {
     console.log('In the ssl');
-    return https.createServer(api);
-};
 
+    const options = {
+        key: fs.readFileSync('/etc/letsencrypt/live/api.sinclairgroup.org/privkey.pem'), // replace with the path to your private key
+        cert: fs.readFileSync('/etc/letsencrypt/live/api.sinclairgroup.org/fullchain.pem') // replace with the path to your certificate
+    };
+
+    return https.createServer(options, api);
+};
