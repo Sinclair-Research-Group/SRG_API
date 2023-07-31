306e8db1fc33ed12a9fc6d009b0641136ee93018
Sinclair-Research-Group <alana@sinclairgroup.org>
2023-07-31 15:58:00 -0700

    changed mysqlconnect file

--- database/mySQLconnect.js
+++ database/mySQLconnect.js
@@ -1,12 +1,16 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-        debug: false,
-        host: 'srg-database-mysql-do-user-14371739-0.b.db.ondigitalocean.com',
-        port: 25060,
-        user: 'areyna',
-        password: 'AVNS_ACW8YxBDBzPrTOdT03T',
-        database: 'srg'
+var pool = mysql.createPool({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_DATABASE
 });
 
-module.exports = connection;
+pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+        if (error) throw error;
+        console.log('The solution is: ', results[0].solution);
+});
+
+module.exports = pool;
